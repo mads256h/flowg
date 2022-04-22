d1a4bf448e8698128e7150172daac53035ff5102
Mads Mogensen <mail@madsmogensen.dk>
2022-04-22 09:39:05 +0200

    Limit numbers in gcode to 5 fractional digits

--- src/main/java/org/flowsoft/flowg/BigDecimalUtils.java
+++ src/main/java/org/flowsoft/flowg/BigDecimalUtils.java
@@ -3,9 +3,26 @@ package org.flowsoft.flowg;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public final class BigDecimalUtils {
     public static final MathContext DEFAULT_MATH_CONTEXT = new MathContext(100, RoundingMode.HALF_UP);
+
+    private static final DecimalFormatSymbols DECIMAL_FORMAT_SYMBOLS = new DecimalFormatSymbols() {
+        {
+            setDecimalSeparator('.');
+        }
+    };
+
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat() {
+        {
+            setMaximumFractionDigits(5);
+            setMinimumFractionDigits(0);
+            setGroupingUsed(false);
+            setDecimalFormatSymbols(DECIMAL_FORMAT_SYMBOLS);
+        }
+    };
 
     public static BigDecimal Divide(BigDecimal left, BigDecimal right) {
         try {
@@ -15,6 +32,10 @@ public final class BigDecimalUtils {
         catch (ArithmeticException e) {
             return left.divide(right, DEFAULT_MATH_CONTEXT);
         }
+    }
+
+    public static String ToGCode(BigDecimal v) {
+        return DECIMAL_FORMAT.format(v);
     }
 
     public static Boolean GreaterThan(BigDecimal left, BigDecimal right) {
